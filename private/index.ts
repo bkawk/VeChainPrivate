ac6feb331a4ebb29ebc68aea9c47129d72dd99da
bkawk <email.will.in.china@gmail.com>
2021-01-15 21:11:36 +0000

    sqs basic setup

--- private/index.ts
+++ private/index.ts
@@ -2,12 +2,16 @@ import * as dotenv from 'dotenv';
 import { BigNumber } from 'bignumber.js';
 import { mnemonic, secp256k1, Transaction } from 'thor-devkit';
 import { thorify } from 'thorify';
+import {
+  SQSClient,
+  ReceiveMessageCommand,
+  DeleteMessageCommand,
+} from '@aws-sdk/client-sqs';
 
 dotenv.config();
-// listner for SQS
 
-// TODO: Make a test / main toggle
 const createTransaction = async (to: string, amount: number) => {
+  // TODO: Make a test / main toggle
   // TODO: Check there is enough funds in the wallet before trying to send.
   const Web3 = require('web3');
   const web3 = thorify(new Web3(), 'https://sync-testnet.veblocks.net');
@@ -19,7 +23,9 @@ const createTransaction = async (to: string, amount: number) => {
     const privateKey = mnemonic.derivePrivateKey(words);
     const blockRef = await web3.eth.getBlockRef();
     const chainTag = await web3.eth.getChainTag();
-    const value = new BigNumber(amount).times(1e18).toFixed(0, BigNumber.ROUND_FLOOR);
+    const value = new BigNumber(amount)
+      .times(1e18)
+      .toFixed(0, BigNumber.ROUND_FLOOR);
     const clauses = [{ data: '0x', to, value }];
     const gas = Transaction.intrinsicGas(clauses);
     const body: Transaction.Body = {
@@ -46,4 +52,43 @@ const createTransaction = async (to: string, amount: number) => {
   }
 };
 
-createTransaction('0x224474d7af5a80708A36DaE803CB4477177A95DE', 1);
+
+const REGION = process.env.SQS_REGION;
+const queueURL = process.env.SQS_QUEUE_URL;
+const params = {
+  AttributeNames: ['SentTimestamp'],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: ['All'],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0,
+};
+const sqs = new SQSClient({ region: REGION });
+
+const run = async () => {
+  try {
+    const data = await sqs.send(new ReceiveMessageCommand(params));
+    if (data.Messages) {
+      const address = 'data.Messages'; // TODO: link this up
+      const value = 1; // TODO: link this up
+      createTransaction(address, value);
+      const deleteParams = {
+        QueueUrl: queueURL,
+        ReceiptHandle: data.Messages[0].ReceiptHandle,
+      };
+      try {
+        await sqs.send(new DeleteMessageCommand(deleteParams));
+      } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.log('Message Deleted', data);
+      }
+    } else {
+      // tslint:disable-next-line: no-console
+      console.log('No messages to delete');
+    }
+  } catch (err) {
+    // tslint:disable-next-line: no-console
+    console.log('Receive Error', err);
+  }
+};
+run();
